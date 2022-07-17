258670bcb9529e019790f30f1b9f872e24c27415
김민철 <kmc901206@gmail.com>
2022-07-17 15:08:08 +0900

    ViewPager2 어댑터 제작 완성

--- app/src/main/java/com/neppplus/todolist_recyclerview_20220717/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/neppplus/todolist_recyclerview_20220717/adapters/MainViewPagerAdapter.kt
@@ -1,18 +1,20 @@
 package com.neppplus.todolist_recyclerview_20220717.adapters
 
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.neppplus.todolistpractice_20220717.fragments.GoalFragment
 import com.neppplus.todolistpractice_20220717.fragments.SettingFragment
 import com.neppplus.todolistpractice_20220717.fragments.TodoFragment
 
-class MainViewPagerAdapter (fm : FragmentManager) : FragmentPagerAdapter(fm) {
-    override fun getCount(): Int {
+class MainViewPagerAdapter (fa : FragmentActivity) : FragmentStateAdapter(fa) {
+    override fun getItemCount(): Int {
         return 3
     }
 
-    override fun getItem(position: Int): Fragment {
+    override fun createFragment(position: Int): Fragment {
         return when (position) {
             0 -> TodoFragment()
             1 -> GoalFragment()
@@ -20,11 +22,4 @@ class MainViewPagerAdapter (fm : FragmentManager) : FragmentPagerAdapter(fm) {
         }
     }
 
-    override fun getPageTitle(position: Int): CharSequence? {
-        return when (position) {
-            0 -> "TodoList"
-            1 -> "Goal"
-            else -> "Setting"
-        }
-    }
 }
