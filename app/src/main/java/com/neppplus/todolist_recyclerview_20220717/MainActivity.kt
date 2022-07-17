f31e7b8714d9a044677e4819c28c62dc9af8945c
김민철 <kmc901206@gmail.com>
2022-07-17 15:22:25 +0900

    ViewPager2 / BottomNav 연동 이벤트 처리 완료

--- app/src/main/java/com/neppplus/todolist_recyclerview_20220717/MainActivity.kt
+++ app/src/main/java/com/neppplus/todolist_recyclerview_20220717/MainActivity.kt
@@ -2,12 +2,13 @@ package com.neppplus.todolist_recyclerview_20220717
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
 import com.neppplus.todolist_recyclerview_20220717.adapters.MainViewPagerAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var mPagerAdapter : MainViewPagerAdapter
+    lateinit var mPagerAdapter: MainViewPagerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +16,25 @@ class MainActivity : AppCompatActivity() {
 
         mPagerAdapter = MainViewPagerAdapter(this)
         mainViewPager.adapter = mPagerAdapter
+
+//        바텀 네비게이션 클릭 이벤트 처리
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> mainViewPager.currentItem = 0
+                R.id.paper -> mainViewPager.currentItem = 1
+                R.id.profile -> mainViewPager.currentItem = 2
+            }
+            return@setOnItemSelectedListener true
+        }
+
+//        뷰 페이저 연결
+        mainViewPager.registerOnPageChangeCallback(
+            object : ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    bottomNav.menu.getItem(position).isChecked = true
+                }
+            }
+        )
     }
 }
